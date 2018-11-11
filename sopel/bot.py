1354e65a68e381810370c49a3390bdcac2cf60f0
Jeffrey Roberts <jeffrey.l.roberts@gmail.com>
2018-11-11 09:49:58 -0500

    Removed Flood Protection

--- sopel/bot.py
+++ sopel/bot.py
@@ -320,7 +320,6 @@ class Sopel(irc.Bot):
             recipient_id = Identifier(recipient)
 
             self.write(('PRIVMSG', recipient), text)
-            self.stack[recipient_id] = self.stack[recipient_id][-10:]
         finally:
             self.sending.release()
         # Now that we've sent the first part, we need to send the rest. Doing
